3508380e89fbd8e7b1b9f55caf1a8c6119e799a8
黃啟嘉 <AlanHuang@hotaiconnected.com.tw>
2026-03-10 19:01:51 +0800

    Fix: Change working directory in start.sh to resolve 404

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# 1. 啟動 WASHDAY
-# --contentRoot 指定程式檔案路徑，--urls 指定監聽位址
-./washday/"WASHDAY 202508" --urls "http://localhost:5001" --contentRoot /app/washday &
+# 1. 先進入洗衣店資料夾再啟動，這樣它才找得到 wwwroot 和 Pages
+cd /app/washday
+ASPNETCORE_URLS="http://localhost:5001" "./WASHDAY 202508" &
 
-# 2. 啟動 CebuCrmApi (門口守衛)
-# 這裡一樣指定路徑，並使用 Render 分配的 $PORT
-./cebucrm/CebuCrmApi --urls "http://0.0.0.0:$PORT" --contentRoot /app/cebucrm
+# 2. 回到 CRM 資料夾啟動大門口
+cd /app/cebucrm
+ASPNETCORE_URLS="http://0.0.0.0:$PORT" ./CebuCrmApi
