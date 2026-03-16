84d1249fecd20a50237da78878859f10d54c2843
黃啟嘉 <AlanHuang@hotaiconnected.com.tw>
2026-03-16 18:33:58 +0800

    i don't know WTF version is this 加上.dockerignore

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@ set -e
 
 echo "Starting WASHDAY..."
 cd /app/washday
-ASPNETCORE_URLS=http://127.0.0.1:5001 ./WASHDAY &
+ASPNETCORE_URLS=http://127.0.0.1:5001 dotnet WASHDAY.dll &
 
 echo "Starting CebuCrmApi..."
 cd /app/cebucrm
-ASPNETCORE_URLS=http://0.0.0.0:$PORT ./CebuCrmApi
+ASPNETCORE_URLS=http://0.0.0.0:$PORT dotnet CebuCrmApi.dll
