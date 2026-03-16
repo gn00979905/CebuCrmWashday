26ef06989a8002338cf2cdc7365ddfacc2008a79
黃啟嘉 <AlanHuang@hotaiconnected.com.tw>
2026-03-16 14:45:18 +0800

    終極解決方案：只要加 cd 切換資料夾就好！ 我們只需要在執行程式之前，先「走進」那個資料夾，再執行程式，一切的設定檔和網頁檔就會完美歸位！

--- start.sh
+++ start.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# 1. 啟動洗衣店：
-# 【注意】不再使用 dotnet 指令！直接執行 Linux 原生檔案，並強制綁定 127.0.0.1
-ASPNETCORE_URLS="http://127.0.0.1:5001" ./washday/WASHDAY &
+# 1. 走進洗衣店的資料夾，啟動洗衣店
+cd /app/washday
+ASPNETCORE_URLS="http://127.0.0.1:5001" ./WASHDAY &
 
-# 2. 稍微等待 3 秒，讓洗衣店 (分機) 完全開機並跑完 Migrate
+# 2. 回到上一層，稍微等個 3 秒
+cd /app
 sleep 3
 
-# 3. 啟動 CRM 總機：
-# 【注意】同樣直接執行原生檔案！並乖乖吃 Render 分配的 PORT
+# 3. 走進總機的資料夾，啟動總機
+cd /app/cebucrm
 export ASPNETCORE_URLS="http://+:$PORT"
-./cebucrm/CebuCrmApi
+./CebuCrmApi
