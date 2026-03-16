35eb28e40db25ed80583d1ddad249746ebc7cc47
黃啟嘉 <AlanHuang@hotaiconnected.com.tw>
2026-03-16 15:29:52 +0800

    先做一個非常重要的 Debug

--- start.sh
+++ start.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
+set -e
 
-# 1. 走進洗衣店的資料夾，啟動洗衣店
+echo "Starting WASHDAY..."
 cd /app/washday
-ASPNETCORE_URLS="http://127.0.0.1:5001" ./WASHDAY &
-
-# 2. 回到上一層，稍微等個 3 秒
-cd /app
-sleep 3
-
-# 3. 走進總機的資料夾，啟動總機
-cd /app/cebucrm
-export ASPNETCORE_URLS="http://+:$PORT"
-./CebuCrmApi
+ASPNETCORE_URLS=http://127.0.0.1:5001 dotnet WASHDAY.dll
