3bf5db9a6d99b01d7a8f168abadd9b3117bba14f
黃啟嘉 <AlanHuang@hotaiconnected.com.tw>
2026-03-16 13:54:45 +0800

    fix 8

--- start.sh
+++ start.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -e
 
+echo "===== WASHDAY FILES ====="
+ls -R /app/washday
+
 echo "Starting WASHDAY..."
 cd /app/washday
-ASPNETCORE_URLS=http://127.0.0.1:5001 dotnet WASHDAY.dll &
+ASPNETCORE_URLS=http://127.0.0.1:5001 ./WASHDAY &
+
+sleep 3
 
 echo "Starting CebuCrmApi..."
 cd /app/cebucrm
-ASPNETCORE_URLS=http://0.0.0.0:$PORT exec ./CebuCrmApi
+ASPNETCORE_URLS=http://0.0.0.0:$PORT ./CebuCrmApi
