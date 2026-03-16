33cba74512984b6a2bd38a46c9bd38e986dac846
黃啟嘉 <AlanHuang@hotaiconnected.com.tw>
2026-03-16 13:10:43 +0800

    fix 5?6?

--- start.sh
+++ start.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
+set -e
 
-# 1. 啟動洗衣店 (WASHDAY)
+echo "Starting WASHDAY..."
 cd /app/washday
-# 既然已經改名沒空格了，"./WASHDAY" 的引號可加可不加，建議保持一致
-ASPNETCORE_URLS="http://127.0.0.1:5001" dotnet WASHDAY/WASHDAY.dll &
+ASPNETCORE_URLS=http://127.0.0.1:5001 dotnet WASHDAY.dll &
 
-# 2. 啟動 CRM 大門 (CebuCrmApi)
+echo "Starting CebuCrmApi..."
 cd /app/cebucrm
-# 使用 exec 可以讓這個程序接收 Render 的關閉訊號，部署會更穩定
-ASPNETCORE_URLS="http://0.0.0.0:$PORT" exec ./CebuCrmApi
+ASPNETCORE_URLS=http://0.0.0.0:$PORT exec ./CebuCrmApi
