d51997a52646686fc5b681cf52f857e6ddc9a6e1
黃啟嘉 <AlanHuang@hotaiconnected.com.tw>
2026-03-16 11:22:43 +0800

    fix 3

--- start.sh
+++ start.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# 1. 啟動洗衣店 (WASHDAY)
-cd /app/washday
-# 既然已經改名沒空格了，"./WASHDAY" 的引號可加可不加，建議保持一致
-ASPNETCORE_URLS="http://127.0.0.1:5001" ./WASHDAY &
+# 1. 啟動洗衣店：強制覆蓋 ASPNETCORE_URLS，並明確指定 127.0.0.1 (不要用 localhost)
+ASPNETCORE_URLS="http://127.0.0.1:5001" dotnet WASHDAY/WASHDAY.dll &
 
-# 2. 啟動 CRM 大門 (CebuCrmApi)
-cd /app/cebucrm
-# 使用 exec 可以讓這個程序接收 Render 的關閉訊號，部署會更穩定
-ASPNETCORE_URLS="http://0.0.0.0:$PORT" exec ./CebuCrmApi
+# 2. 稍微等待 3 秒，確保洗衣店完全啟動
+sleep 3
+
+# 3. 啟動總機 CRM：不加前置變數，讓它乖乖吃 Render 分配的 10000 Port
+dotnet cebucrm/CebuCrmApi.dll   # (請保留你原本啟動 CRM 的 dll 路徑)
